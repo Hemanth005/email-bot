f47e0b3303232141d480b41dd51b06d1407d02ae
Hemanth005 <hemanth005239@gmail.com>
2021-01-07 03:58:52 +1100

    changes made successfully

--- email_bot.py
+++ email_bot.py
@@ -1,14 +1,14 @@
 import smtplib
 from email.message import EmailMessage
 email = EmailMessage()
-email['from'] = 'hemanth rao'
-email['to'] = 'tejdeep20012013@gmail.com'
-email['subject'] = 'Hi i am just a robot created by hemanth'
-email.set_content('Just testing things out...on your demand...HAPPY BIRTHDAY')
+email['from'] = 'Your Name'
+email['to'] = 'XXXXXXX@gmail.com'
+email['subject'] = 'Hi i am just a robot created by xxxx'
+email.set_content('Just testing things out')
 
 with  smtplib.SMTP(host='smtp.gmail.com', port = 587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('raohemanth316@gmail.com', 'No password@123')
+	smtp.login('emailid@gmail.com', 'Your Password')
 	smtp.send_message(email)
 	print("All done!!!")
